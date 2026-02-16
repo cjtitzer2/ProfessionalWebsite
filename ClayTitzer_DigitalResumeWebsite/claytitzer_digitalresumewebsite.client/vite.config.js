1aa52bfe60b6d1189a7a772adcdfa835147cfd80
cjtitzer2 <titzerclay@gmail.com>
2026-02-16 14:23:10 -0600

    Make Pages deploy path-agnostic with relative Vite base

--- ClayTitzer_DigitalResumeWebsite/claytitzer_digitalresumewebsite.client/vite.config.js
+++ ClayTitzer_DigitalResumeWebsite/claytitzer_digitalresumewebsite.client/vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const defaultBasePath = repositoryName ? `/${repositoryName}/` : '/'
-const basePath = process.env.VITE_BASE_PATH ?? (process.env.GITHUB_ACTIONS === 'true' ? defaultBasePath : '/')
+const basePath = process.env.VITE_BASE_PATH ?? './'
 
 // https://vite.dev/config/
 export default defineConfig({
